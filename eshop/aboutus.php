fcbaaf283633ffc32fde39165c019b41bf0793f3
carollee77 <manlee77@yahoo.com.hk>
2018-05-01 13:16:17 +0800

    Add files via upload

---
+++ eshop/aboutus.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <title>BookLovers Online Store</title>
+ <link href="css/online.css" rel="stylesheet" type="text/css" />
+ <style type="text/css" media="all">
+ @import "css/online.css";
+ </style>
+</head>
+<body class="twoColFixLtHdr">
+<div id="header">
+  <!-- end #header -->
+</div>
+<div id="container">
+<div id="meun"><a href="../eshop/booklovers/index.php">BookLovers Home</a>
+
+  
+ <a href="../eshop/booklovers/contact.php">Contact</a>
+    <a href="../eshop/booklovers/aboutus.php">About us</a>
+    
+     <br></div>
+ <div align="center">  <img src="../eshop/images/finnal2.gif" alt="Online Book Store" width="950" height="200" /></div>
+  <div id="container1"></div>
+  <div id="sidebar1">
+    <div id="subsidebar1">
+      <div id="subsidebar3"> Navigation </div>
+      <div id="subsidebar2"><a href="index.php">Home</a>
+      </div>
+      <div id="subsidebar2"><a href="signup.php">Register</a>
+      </div>
+      <div id="subsidebar2"><a href="login.php">Login</a>
+      </div>
+   
+    </div>
+    <!-- end #sidebar1 -->
+  </div>
+  <div id="mainContent">
+    <div id="mainContent1">
+    <div id="middletxtheadermain">
+      <div id="middletxtheader" align="right">Welcome</div>
+      <div id="middletxt1">
+       <div align="center"><img src="images/onlinebanner.jpeg" alt="Online Shopping" width="600" height="100" /> </div>
+      </div>
+      </div>
+      <div id="middletxt">
+       <div id="middletxtheader" align="right">About Online Shopping System </div>
+       <span align="left"><p align="left"><strong><font size="2">
+        The online store application is designed to be a generic online ecommerce platform. Multiple vendors can register to use the platform to sell products online.</strong>
+        <ul>
+          <li><strong> Product Management :</strong></li>
+          <p>Product Management includes the information about particular product,such as product number,
+          item, name, category, images of products, description, features, constraints of products.
+          All these will be entered to the database through Product Management and hence made available in the website.
+          In Other words Product Management is the admin area for the vendors/sellers where they can put information about their products which are to be
+          displayed in the website.</p>
+          <li><strong> Price Management : </strong></li>
+          <p>Price Management deals with the cost of the product, discounts applicable for the particular product of a vendor/seller.
+           Discounts may be seasonal discounts, promotional discounts or any.</p>
+          <li><strong> Reporting :</strong></li>
+          <p>In Reporting all reports will be generated. Whenever a item is sold, or customer orders a product,
+           its vendor will be sent an alert via email immediately so that he can ship that item soon.</p>
+
+        </ul></font>
+       </p></span>
+        <!-- end #middletxt -->
+      </div>
+    </div>
+    <!-- end #mainContent -->
+  </div>
+  <!-- This clearing element should immediately follow the #mainContent div in order to force the #container div to contain all child floats -->
+  <div id="footer">
+    (C) 2018 OurSite. Design by BookLovers Limited
+    <!-- end #footer -->
+  </div>
+  <!-- end #container -->
+</div>
+</body>
+</html>
