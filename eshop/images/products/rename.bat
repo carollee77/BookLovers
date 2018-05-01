cc8631e1eb324f0d1161ecd387af15725d925ea0
carollee77 <manlee77@yahoo.com.hk>
2018-05-01 13:33:00 +0800

    Add files via upload

---
+++ eshop/images/products/rename.bat
@@ -0,0 +1,56 @@
+rename "dictionary1.jpg" 201.jpg
+rename "The Complete Uxbridge English Dictionary.jpg" 202.jpg
+rename "Win Win.JPG" 203.jpg
+rename "Collins Gem German School Dictionary.jpg" 204.jpg
+rename "pic115.jpg" 205.jpg
+rename "Camb Essential Eng Dictionary 2ed.jpg" 206.jpg
+rename "Getting There.jpg" 207.jpg
+rename "comp8.jpg" 208.jpg
+rename "comp9.jpg" 209.jpg
+rename "High Street Heroes.jpg" 210.jpg
+rename "Whos That Ant Whose Dead End.jpg" 211.jpg
+rename "pic111.jpg" 212.jpg
+rename "pic114.jpg" 213.jpg
+rename "Who Am I.jpg" 214.jpg
+rename "travel2.jpg" 215.jpg
+rename "020135358X.gif" 216.jpg
+rename "biology.gif" 217.jpg
+rename "bookoftea.jpg" 218.jpg
+rename "cofee.jpg" 219.jpg
+rename "stonesoup.jpg" 220.jpg
+rename "pasta1.jpg" 221.jpg
+rename "bhartiya.jpg" 222.jpg
+rename "cofee2.jpg" 223.jpg
+rename "tax1.jpg" 224.jpg
+rename "tax2.jpg" 225.jpg
+rename "tax3.jpg" 226.jpg
+rename "pic119.jpg" 227.jpg
+rename "pic118.jpg" 228.jpg
+rename "comp2.jpg" 229.jpg
+rename "pic112.jpg" 230.jpg
+rename "pic113.jpg" 231.jpg
+rename "COMPETITIVE8.jpg" 232.jpg
+rename "p2.jpg" 233.jpg
+rename "p6.jpg" 234.jpg
+rename "ART3.JPG" 235.jpg
+rename "busi7.jpg" 236.jpg
+rename "comp7.jpg" 237.jpg
+rename "business.jpg" 238.jpg
+rename "lawyer.jpg" 239.jpg
+rename "cul1.jpg" 240.jpg
+rename "CAT.jpg" 241.jpg
+rename "yoga1.jpg" 242.jpg
+rename "9062999.jpg" 243.jpg
+rename "1445.jpg" 244.jpg
+rename "t2.jpg" 245.jpg
+rename "comic1.jpg" 246.jpg
+rename "fic1.jpg" 247.jpg
+rename "med2.jpg" 248.jpg
+rename "MANAGEMENT2.jpg" 249.jpg
+rename "terr2.jpg" 250.jpg
+rename "comp10.jpg" 251.jpg
+rename "Paris for One (Quick Reads).jpg" 252.jpg
+rename "The Music Shop.jpg" 253.jpg
+rename "Year of the Death of Ricardo Reis Panther.jpg" 254.jpg
+rename "Zuckerman Unbound.jpg" 255.jpg
+rename "Wolf in White Van.jpg" 256.jpg
